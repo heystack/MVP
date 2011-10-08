3167c174d9588afbd87c34349c8a639f10111812
StkUp <nycbrown@gmail.com>
2011-10-08 11:11:22 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mvp::Application.config.secret_token = 'bcae40c4a0d29619c979290ce360720459845318d8604dfe5727f89e356368e547c65ddd7fcf5ec460662c29ad1eadec37d260ebe1b028a31ae617b834cd4d6f'
