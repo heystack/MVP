ff45f091c7bc172d4e57e0db6094e94744731d24
StkUp <nycbrown@gmail.com>
2011-10-21 17:34:55 -0400

    Resolving cookie sessions issue

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,6 +2,10 @@
 
 # Mvp::Application.config.session_store :cookie_store, :key => '_mvp_session'
 Mvp::Application.config.session_store :active_record_store, :key => '_mvp_session'
+Mvp::Application.config.action_dispatch.session = {
+  :key    => '_mvp_session',
+  :secret => '0x0dkfj3927dkc7djdh36rkckdfzsgkc7djdh36rkckdfzsg'
+}
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
