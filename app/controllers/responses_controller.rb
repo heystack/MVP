808b20cc5e7c830d7b572878562b9ce3e3187a8e
StkUp <nycbrown@gmail.com>
2011-11-02 00:53:12 -0400

    Formatting changes

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -52,7 +52,7 @@ class ResponsesController < ApplicationController
   def stkresponses
     @responses = Response.all(:order => 'id DESC')
     @count = Response.count
-    @topics = Topic.group('id')
+    @topics = Topic.group('id', 'name')
   end
 
 end
