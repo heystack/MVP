d51c7bb000160addd60afae4d0a419d91a712789
StkUp <nycbrown@gmail.com>
2011-11-11 14:44:10 -0500

    Added auth_key on topics::show

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -33,7 +33,26 @@ class TopicsController < ApplicationController
 
     if !session[:you]
       # session[:email] ||= params[:email]
-      redirect_to new_topic_response_path(@topic) and return
+      if params[:auth_key]
+        if @topic.name == "Babysitter Pay Rate"
+          session[:babysitter_pay_rate] = 15
+          session[:babysitter_id] = 1
+          session[:you] = session[:babysitter_pay_rate]
+          session[:response_id] = session[:babysitter_id]
+        elsif @topic.name == "Mobilizers"
+          session[:mobilizers] = 11.5
+          session[:mobilizers_id] = 2
+          session[:you] = session[:mobilizers]
+          session[:response_id] = session[:mobilizers_id]
+        elsif @topic.name == "Homework"
+          session[:homework] = 3
+          session[:homework_id] = 3
+          session[:you] = session[:homework]
+          session[:response_id] = session[:homework_id]
+        end
+      else
+        redirect_to new_topic_response_path(@topic) and return
+      end
     else
       session[:you] ||= 0
     end
